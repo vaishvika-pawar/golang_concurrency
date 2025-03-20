c85152940390b915e6f2a2a0cbd0a2b8ef07d2b2
vaishvika <vaishvika.pawar@kotak.com>
2025-03-20 22:26:10 +0530

    using slices to get outour from all function through multiple channels

--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
 )
 
 func main() {
-	done := make(chan bool)
-	go greet("Nice to meet you!", done)
-	go greet("How are you?", done)
-	go slowGreet("How ... are ... you ... ?", done)
-	go greet("I hope you're liking the course!", done)
-	<-done
-	<-done
-	<-done
-	<-done
+	dones := make([]chan bool, 4)
+	//done := make(chan bool)
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you!", dones[0])
+	dones[1] = make(chan bool)
+	go greet("How are you?", dones[1])
+	dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ... ?", dones[2])
+	dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", dones[3])
+
+	for _, done := range dones {
+		<-done
+	}
 	// All functions start after the function before is executed completely
 	// To run then in parallel we add go keyword before the function call
 	// this indicates to call go routines
